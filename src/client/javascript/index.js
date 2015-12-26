f7720063f3c3a5c25e3dd3e6c4a12bd922fb4187
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2015-12-26 17:29:59 +0100

    Get the subscriptions from front end

--- src/client/javascript/index.js
+++ src/client/javascript/index.js
@@ -1,18 +1,44 @@
 var socket = io('http://localhost:9000');
 
 var btn = document.getElementById('open-auth-window');
+var status = document.getElementById('status');
 
 var openAuthWindow = function (_btn) {
-  _btn.toElement.innerHTML = 'Loading...';
   _btn.toElement.disabled = true;
+
+  status.innerHTML = 'Loading...';
 
   socket.emit('youtube/auth');
 }
 
-socket.on('youtube/callback', function () {
-  document.getElementById('main').innerHTML = 'Connected to the YouTube API :-)';
+socket.on('youtube/asked', function () {
+  status.innerHTML = 'Waiting for YouTube response...';
+});
 
-  socket.emit('youtube/subscriptions');
+socket.on('youtube/waitingforuser', function () {
+  status.innerHTML = 'Please fill in the required informations in the other window';
+});
+
+socket.on('youtube/callback', function (token) {
+  var main = document.getElementById('main');
+  main.innerHTML = '<h1>Connected to the YouTube API :-)</h1>5 subscriptions:<br />';
+
+  gapi.auth.setToken(token);
+  gapi.client.load('youtube', 'v3', function () {
+    // Retrieve user's subscriptions
+    gapi.client.youtube.subscriptions.list({
+      part: 'id, snippet',
+      mine: true
+    }).execute(function (subscriptions) {
+      main.innerHTML = main.innerHTML + '<ul>';
+
+      subscriptions.items.forEach(function (subscription) {
+        main.innerHTML = main.innerHTML + '<li>' + subscription.snippet.title + '</li>';
+      });
+
+      main.innerHTML = main.innerHTML + '</ul>';
+    });
+  });
 });
 
 btn.onclick = openAuthWindow;
--- src/youtubeapi.js
+++ src/youtubeapi.js
@@ -1,19 +1,13 @@
-var readline = require('readline');
 var credentials = require('./credentials');
 
 var google = require('googleapis');
 var OAuth2Client = google.auth.OAuth2;
-var Youtube = google.youtube('v3');
 
 const REDIRECT_URL = 'http://localhost:9000/youtube/callback';
 const oauth2Client = new OAuth2Client(credentials.CLIENT_ID, credentials.CLIENT_SECRET, REDIRECT_URL);
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function getAuthUrl(cb) {
+// retrieve the auth page url
+module.exports.getAuthUrl = function (cb) {
   // generate consent page url
   var url = oauth2Client.generateAuthUrl({
     access_type: 'offline', // will return a refresh token
@@ -21,30 +15,12 @@ function getAuthUrl(cb) {
   });
 
   return cb(url);
-}
-
-function getToken(code, cb) {
-  // request access token
-  oauth2Client.getToken(code, function(err, tokens) {
-    // set tokens to the client
-    oauth2Client.setCredentials(tokens);
-    cb();
-  });
-}
+};
 
 // retrieve an access token
-module.exports.getAuthUrl = function (cb) {
-  getAuthUrl(cb);
-};
-
 module.exports.getToken = function (code, cb) {
-  getToken(code, cb);
-};
-
-module.exports.getSubscriptions = function (cb) {
-  Youtube.subscriptions.list({
-    part: 'id',
-    home: true,
-    auth: oauth2Client
-  }, cb);
+  // request access token
+  oauth2Client.getToken(code, function(err, token) {
+    cb(token);
+  });
 };
