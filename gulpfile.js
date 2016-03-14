a05da7447a186e4802fc047f77cf0f1c82bc6175
Yann Bertrand <yann@bertrand.com>
2016-03-14 22:41:09 +0100

    Use gulp-babel to load multiple client js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,36 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
+var debug = require('gulp-debug');
 var electron = require('electron-connect').server.create();
+var async = require('async');
 
-gulp.task('electron:restart', function() {
+gulp.task('electron:start', ['transpile', 'copy'], function() {
+  electron.start();
+});
+
+gulp.task('electron:restart', ['transpile', 'copy'], function() {
   electron.restart();
 });
 
-gulp.task('watch', function () {
-  electron.start();
-  gulp.watch('./src/**/*.html', ['electron:restart']);
-  gulp.watch('./src/**/*.js', ['electron:restart']);
-  gulp.watch('./src/**/*.css', ['electron:restart']);
+gulp.task('watch', ['electron:start'], function () {
+  gulp.watch(['src/**/*'], ['transpile', 'copy', 'electron:restart']);
 });
 
-// gulp
-gulp.task('default', [
-  'watch'
-]);
+gulp.task('transpile', function (cb) {
+  gulp.src(['src/**/*.js'])
+    .pipe(debug())
+    .pipe(babel({
+      presets: ['es2015', 'react']
+    }))
+    .pipe(gulp.dest('dist'))
+    .on('end', cb);
+});
+
+gulp.task('copy', function (cb) {
+  gulp.src(['src/**/*', '!src/**/*.js'])
+    .pipe(debug())
+    .pipe(gulp.dest('dist'))
+    .on('end', cb);
+});
+
+gulp.task('default', ['transpile', 'copy', 'electron:start', 'watch']);
