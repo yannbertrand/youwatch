34edc6ba76d6011f352eec7471d2a41c6e0caa29
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-03-16 20:38:21 +0100

    Check the port before running the app

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var cache = require('gulp-cached');
 var replace = require('gulp-replace-task');
 var electron = require('electron-connect').server.create();
 var async = require('async');
+var tcpPortUsed = require('tcp-port-used');
 var CONFIG = require('./src/config');
 
 var replaceOptions = {
@@ -15,6 +16,28 @@ var replaceOptions = {
     }
   ]
 };
+
+var isPortUsed = function (port, cb) {
+  tcpPortUsed
+    .check(port, '127.0.0.1')
+    .then(function (inUse) {
+      cb(null, inUse);
+    }, function (err) {
+      cb(err);
+  });
+};
+
+gulp.task('check-port', function (cb) {
+  isPortUsed(CONFIG.PORT, function (err, portInUse) {
+    if (err) {
+      return cb('An unknown error occured');
+    }
+    if (portInUse)
+      return cb('The port ' + CONFIG.PORT + ' is already in use');
+  
+    cb();
+  });
+});
 
 gulp.task('electron:start', ['transpile', 'copy'], function() {
   electron.start();
@@ -28,7 +51,7 @@ gulp.task('watch', ['electron:start'], function () {
   gulp.watch(['src/**/*'], ['transpile', 'copy', 'electron:restart']);
 });
 
-gulp.task('transpile', function (cb) {
+gulp.task('transpile', ['check-port'], function (cb) {
   gulp.src(['src/**/*.js'])
     .pipe(cache('transpile'))
     .pipe(debug())
@@ -40,7 +63,7 @@ gulp.task('transpile', function (cb) {
     .on('end', cb);
 });
 
-gulp.task('copy', function (cb) {
+gulp.task('copy', ['check-port'], function (cb) {
   gulp.src(['src/**/*.*', '!src/**/*.js'])
     .pipe(cache('copy'))
     .pipe(debug())
@@ -49,4 +72,4 @@ gulp.task('copy', function (cb) {
     .on('end', cb);
 });
 
-gulp.task('default', ['transpile', 'copy', 'electron:start', 'watch']);
+gulp.task('default', ['check-port', 'transpile', 'copy', 'electron:start', 'watch']);
