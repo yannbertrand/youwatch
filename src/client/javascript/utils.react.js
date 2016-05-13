50ba89fed3d7d57583bba62ef29ea2f0065aefeb
Yann Bertrand <yann@bertrand.com>
2016-05-13 18:47:15 +0200

    Correct the timer element issue

--- src/client/javascript/utils.react.js
+++ src/client/javascript/utils.react.js
@@ -3,14 +3,22 @@ const Timer = React.createClass({
     return { secondsToWait: this.props.secondsToWait };
   },
   runTimer: function () {
-    let timer = setInterval(() => {
-      if (this.state.secondsToWait === 0) clearInterval(timer);
+    this.setState({
+      timer: setInterval(() => {
+        if (this.state.secondsToWait === 0) clearInterval(timer);
 
-      this.setState({ secondsToWait: this.state.secondsToWait - 1 });
-    }, 1000);
+        this.setState({ secondsToWait: this.state.secondsToWait - 1 });
+      }, 1000)
+    });
+  },
+  stopTimer: function () {
+    clearInterval(this.state.timer);
   },
   componentDidMount: function () {
     this.runTimer();
+  },
+  componentWillUnmount: function () {
+    this.stopTimer();
   },
   render: function () {
     if (this.state.secondsToWait > 0) {
