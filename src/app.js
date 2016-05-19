7a04276351ec32dc8c42ee79c3f3d601176e790f
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-05-19 22:37:52 +0200

    Upgrade Electron and other dependencies, clean package.json

--- src/app.js
+++ src/app.js
@@ -2,33 +2,23 @@
 
 const CONFIG = require('./config');
 
-const app = require('app');
+const electron = require('electron');
+const app = electron.app;
 
 const YoutubeApi = require('./youtubeapi');
-const Windows = require('./windows');
+const Windows = require('./windows')(electron, CONFIG);
 const server = require('./server');
 const database = require('./database');
 
 const isOnline = require('is-online');
 const youtubeRegex = require('youtube-regex');
 
-// report crashes to the Electron project
-require('crash-reporter').start({
-  companyName: 'YouWatch',
-  submitURL: '',
-});
-
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate-with-no-open-windows', Windows.activateWithNoOpenWindows);
-
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
   Windows.openMainWindow();
 
@@ -187,4 +177,21 @@ app.on('ready', () => {
       });
     }
   });
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    Windows.openMainWindow();
+  }
 });
