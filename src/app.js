ebc73a9b695fbacc7a60a23ea49202141bb5fd17
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-06-01 21:28:52 +0200

    Fix a window issue

--- src/app.js
+++ src/app.js
@@ -194,7 +194,5 @@ app.on('window-all-closed', () => {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    Windows.openMainWindow();
-  }
+  Windows.activateWithNoOpenWindows();
 });
