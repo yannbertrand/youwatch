9b58ae4098e6b5707bf5a2ec5a2dd17c0b8652e6
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-06-09 23:25:53 +0200

    Refactor

--- src/resources/channel.js
+++ src/resources/channel.js
@@ -15,11 +15,11 @@ module.exports = function (_async, _YouTube, _oauth2Client, _db) {
   };
 };
 
-function findAllChannels(cb) {
-  db.find({ kind: 'youtube#channel' }, cb);
+function findAllChannels(callback) {
+  db.find({ kind: 'youtube#channel' }, callback);
 }
 
-function refreshChannels(subscriptions, cb) {
+function refreshChannels(subscriptions, callback) {
   let pageToken = true;
   let newChannels = [];
   let updatedChannels = [];
@@ -28,65 +28,50 @@ function refreshChannels(subscriptions, cb) {
 
   async.each(subscriptions, refreshSubscriptionChannel, sendNewAndUpdatedChannels);
 
-
-
   function refreshSubscriptionChannel(subscription, nextSubscription) {
-    YouTube.channels.list(concoctRequest(subscription), gotChannel);
+    YouTube.channels.list(concoctRequest(subscription), handleError(gotChannel));
 
-    function concoctRequest(subscription) {
-      return {
-        part: 'id, snippet, contentDetails',
-        id: subscription.channelId,
-        auth: oauth2Client,
-      };
-    }
-
-    function gotChannel(err, channel) {
-      if (err) {
-        console.error('Error while trying to get channel ' + subscription.channelId, err);
-        return nextSubscription();
-      }
-
+    function gotChannel(channel) {
       if (channel && channel.items && channel.items.length) {
-        upsertChannel(channel.items[0], function (err, newChannel, updatedChannel) {
-          if (newChannel) {
-            newChannels.push(newChannel);
-          }
-          if (updatedChannel) {
-            updatedChannels.push(updatedChannel);
-          }
-          nextSubscription();
-        });
+        upsertChannel(channel.items[0], handleError(upsertedChannel));
       } else {
         nextSubscription();
       }
+    }
+
+    function upsertedChannel(newChannel, updatedChannel) {
+      if (newChannel) newChannels.push(newChannel);
+      if (updatedChannel) updatedChannels.push(updatedChannel);
+      
+      nextSubscription();
     }
   }
 
   function sendNewAndUpdatedChannels(err) {
     console.info('END: refreshChannels');
-    cb(err, newChannels, updatedChannels);
+    callback(err, newChannels, updatedChannels);
   }
 }
 
-function upsertChannel(channel, cb) {
-  console.info('START: upsertChannel');
+function concoctRequest(subscription) {
+  return {
+    part: 'id, snippet, contentDetails',
+    id: subscription.channelId,
+    auth: oauth2Client,
+  };
+}
+
+function upsertChannel(channel, callback) {
+  // console.info('START: upsertChannel');
 
   let request = {
     kind: 'youtube#channel',
     id: channel.id,
   };
 
-  db.findOne(request, gotChannel);
+  db.findOne(request, handleError(gotChannel));
 
-
-
-  function gotChannel(err, result) {
-    if (err) {
-      console.error(err);
-      return sendNewChannels(err);
-    }
-
+  function gotChannel(result) {
     let dbChannel = {
       kind: 'youtube#channel',
       id: channel.id,
@@ -118,14 +103,25 @@ function upsertChannel(channel, cb) {
     }
 
     function sendNewChannels(err, channel) {
-      console.info('END: upsertChannel');
-      cb(err, channel);
+      // console.info('END: upsertChannel');
+      callback(err, channel);
     }
 
     function sendUpdatedChannels(err) {
-      console.info('END: upsertChannel');
-      console.log(dbChannel);
-      cb(err, null, dbChannel);
+      // console.info('END: upsertChannel');
+      callback(err, null, dbChannel);
     }
   }
+}
+
+function handleError(next) {
+  return function (err) {
+    if (err) {
+      console.error(err);
+      return callback(err);
+    }
+
+    let _arguments = Array.prototype.slice.call(arguments, 1);
+    next.apply(null, _arguments);
+  };
 }
