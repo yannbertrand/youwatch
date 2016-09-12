5d27f4dc37d1351e22048dee07155251963f7eae
benavern <bencrdc@gmail.com>
2016-09-12 17:30:50 +0200

    Refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const debug = require('gulp-debug');
-const cache = require('gulp-cached');
-const plumber = require('gulp-plumber');
-const replace = require('gulp-replace-task');
-const notify = require("gulp-notify");
-const electron = require('electron-connect').server.create();
-const async = require('async');
+const gulp        = require('gulp');
+const babel       = require('gulp-babel');
+const debug       = require('gulp-debug');
+const cache       = require('gulp-cached');
+const plumber     = require('gulp-plumber');
+const replace     = require('gulp-replace-task');
+const notify      = require("gulp-notify");
+const electron    = require('electron-connect').server.create();
+const async       = require('async');
 const tcpPortUsed = require('tcp-port-used');
-const CONFIG = require('./src/config');
+const CONFIG      = require('./src/config');
 
-const errorTemplate = '<%= error.message %>';
+/**
+ * Settings
+ */
+const errorTemplate = {
+  title:    "Gulp error!",
+  message:  "<%= error.message %>",
+};
 const replaceOptions = {
   patterns: [
     {
@@ -21,14 +27,20 @@ const replaceOptions = {
 };
 
 
-gulp.task('default', ['transpile', 'copy', 'check-port', 'electron:start', 'watch']);
+/**
+ * Electron tasks
+ */
+gulp.task('electron:start', ['transpile:server', 'transpile:client', 'copy', 'check-port'], () => electron.start());
+gulp.task('electron:restart',         ['transpile:server'], electron.restart);
+gulp.task('electron:reload:htmlcss',  ['copy'],             electron.reload);
+gulp.task('electron:reload:js',       ['transpile:client'], electron.reload);
 
 
-
-gulp.task('electron:start', ['transpile', 'copy', 'check-port'], () => electron.start());
-
-gulp.task('transpile', function (callback) {
-  gulp.src(['src/**/*.js'])
+/**
+ * Files tasks
+ */
+gulp.task('transpile:server', function () {
+  gulp.src(['src/**/*.js', '!src/client/*'])
     .pipe(plumber({ errorHandler: notify.onError(errorTemplate)}))
     .pipe(cache('transpile'))
     .pipe(debug())
@@ -37,31 +49,57 @@ gulp.task('transpile', function (callback) {
       presets: ['es2015', 'react']
     }))
     .pipe(gulp.dest('dist'))
-    .on('end', callback);
 });
 
-gulp.task('copy', function (callback) {
+gulp.task('transpile:client', function () {
+  gulp.src(['src/client/**/*.js'])
+    .pipe(plumber({ errorHandler: notify.onError(errorTemplate)}))
+    .pipe(cache('transpile'))
+    .pipe(debug())
+    .pipe(replace(replaceOptions))
+    .pipe(babel({
+      presets: ['es2015', 'react']
+    }))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('copy', function () {
   gulp.src(['src/**/*.*', '!src/**/*.js'])
     .pipe(plumber({ errorHandler: notify.onError(errorTemplate) }))
     .pipe(cache('copy'))
     .pipe(debug())
     .pipe(replace(replaceOptions))
-    .pipe(plumber.stop())
     .pipe(gulp.dest('dist'))
-    .on('end', callback);
 });
 
-gulp.task('electron:restart', ['transpile'], electron.restart);
-gulp.task('electron:reload', ['copy'], electron.reload);
 
+/**
+ * Watch task
+ */
 gulp.task('watch', function () {
   // Restart electron when server or electron js files change
   gulp.watch(['src/**/*.js', '!src/client/*'], ['electron:restart']);
 
   // Reload electron when client files change
-  gulp.watch(['src/client/**/*.{html,css,js}'], ['electron:reload']);
+  gulp.watch(['src/client/**/*.{html,css}'], ['electron:reload:htmlcss']);
+  gulp.watch(['src/client/**/*.js'], ['electron:reload:js']);
 });
 
+
+
+/** 
+ * ============================================================
+ *   Default task (npm start)
+ *   transpile, copy, ..., then start electron & watch files
+ * ============================================================
+ */
+gulp.task('default', ['electron:start', 'watch']);
+
+
+
+/**
+ * Utils
+ */
 gulp.task('check-port', function (callback) {
   isPortUsed(CONFIG.PORT, function (err, portInUse) {
     if (err)
@@ -72,8 +110,6 @@ gulp.task('check-port', function (callback) {
     callback();
   });
 });
-
-
 
 function isPortUsed(port, callback) {
   tcpPortUsed
