a372a227c8e2b2a63664ebf81f538024ff088a74
benavern <bencrdc@gmail.com>
2016-09-22 00:16:47 +0200

    Fix margins on pages with text

--- src/client/scripts/pages/no-internet.react.js
+++ src/client/scripts/pages/no-internet.react.js
@@ -1,10 +1,12 @@
 const NoInternetPage = React.createClass({
   render: function () {
     return (
-      <div className="jumbotron">
-        <h1 className="display-3">You're not connected to the internet</h1>
-        <p className="lead">Offline mode is not implemented yet</p>
-        <p><i className="fa fa-spinner"></i> Waiting for reconnection...</p>
+      <div className="text-page">
+        <div className="jumbotron">
+          <h1 className="display-3">You're not connected to the internet</h1>
+          <p className="lead">Offline mode is not implemented yet</p>
+          <p><i className="fa fa-spinner"></i> Waiting for reconnection...</p>
+        </div>
       </div>
     );
   }
