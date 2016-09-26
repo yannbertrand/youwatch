82237345751cb726a1ba8652eb78b42a6a1a79bd
Yann Bertrand <yann@bertrand.com>
2016-09-26 21:29:33 +0200

    Store dark theme and layout config in local storage

--- src/client/scripts/pages/configuration.react.js
+++ src/client/scripts/pages/configuration.react.js
@@ -1,32 +1,38 @@
 const ConfigurationPage = React.createClass({
-  toggleDarkTheme : function(event) {
-    var darkTheme = document.body.classList.toggle('dark');
+  toggleDarkTheme: function () {
+    const darkTheme = document.body.classList.toggle('dark');
+
+    localStorage.setItem('darkTheme', castBooleanToString(darkTheme));
 
     this.setState({ darkTheme });
   },
-  changeLayout: function(event) {
+  changeLayout: function (event) {
     let layout = event.target.value;
 
-    if(layout == 'overlay') {
-      document.body.classList.add('layout-overlay');
-      document.body.classList.remove('layout-sticker');
-    }
-    else if(layout == 'sticker') {
-      document.body.classList.remove('layout-overlay');
-      document.body.classList.add('layout-sticker');
-    }
-    else {
-      document.body.classList.remove('layout-overlay');
-      document.body.classList.remove('layout-sticker');
-    }
+    switch (layout) {
+      case 'overlay':
+        document.body.classList.add('layout-overlay');
+        document.body.classList.remove('layout-sticker');
+        localStorage.setItem('layout', 'overlay');
+        break;
+      case 'sticker':
+        document.body.classList.remove('layout-overlay');
+        document.body.classList.add('layout-sticker');
+        localStorage.setItem('layout', 'sticker');
+        break;
+      default:
+        document.body.classList.remove('layout-overlay');
+        document.body.classList.remove('layout-sticker');
+        localStorage.setItem('layout', 'youtube');
+    };
 
     this.setState({ layout: layout });
   },
   getInitialState: () => {
     return {
       showConsole: false,
-      darkTheme : document.body.classList.contains('dark'),
-      layout: document.body.classList.contains('layout-overlay') ? 'overlay' : (document.body.classList.contains('layout-sticker') ? 'sticker' : 'youtube'),
+      darkTheme : isDarkThemeActive(),
+      layout: getActiveLayout(),
     };
   },
   render: function () {
@@ -58,7 +64,7 @@ const ConfigurationPage = React.createClass({
             <label className="col-sm-4">Layout</label>
             <div className="col-sm-8">
               <div className="select">
-                <select className="form-control" value={this.state.layout} onChange={this.changeLayout} >
+                <select className="form-control" value={this.state.layout} onChange={this.changeLayout}>
                   <option value="youtube">Youtube</option>  
                   <option value="overlay">Overlay</option>  
                   <option value="sticker">Sticker</option>
