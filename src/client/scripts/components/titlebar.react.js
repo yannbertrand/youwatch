257e0a9fd6978eb1129354d4019b0610b1104cb6
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-09-30 23:29:33 +0200

    Split Mac and Windows title bar components, fix indention

--- src/client/scripts/components/titlebar.react.js
+++ src/client/scripts/components/titlebar.react.js
@@ -1,65 +1,92 @@
 const {remote} = require('electron');
 
-const Titlebar = React.createClass({
+const MacTitlebar = React.createClass({
 
-  render: function () {
-
-    const w = {
-        min: () => {
-            const win = remote.getCurrentWindow()
-            win.minimize()
-        },
-        max: () => {
-            const win = remote.getCurrentWindow()
-            if (!win.isMaximized()) win.maximize()
-            else win.unmaximize()
-        },
-        quit: () => {
-            const win = remote.getCurrentWindow()
-            win.close()
-        },
-    }
-
-    let titleBarText;
-    if (this.props.platform === 'darwin') {
-        titleBarText = (<div className="titlebar--text">YouWatch</div>);
-    } else {
-        titleBarText = (
-            <div className="titlebar--text">
-                <img className="titlebar--icon" src="images/icon.png" />
-                YouWatch
-            </div>
-        );
-    }
+  render() {
 
     return (
-      
-        <div id="titlebar" data-platform={this.props.platform}>
-        
-            {titleBarText}
 
-            <div className="titlebar--controls-wrapper">
-
-                <div className="titlebar--controls-minimize" onClick={w.min}>
-                    <span></span>                  
-                </div>
-
-                <div className="titlebar--controls-maximize" onClick={w.max}>
-                    <span></span>
-                    <span className="bar2"></span>  
-                </div>
-
-                <div className="titlebar--controls-close" onClick={w.quit}>
-                    <span></span>
-                    <span className="bar2"></span>
-                </div>
-
-            </div>
-            
+      <div id="titlebar" data-platform={this.props.platform}>
+        <div className="titlebar--text">
+          YouWatch
         </div>
+      </div>
 
     );
-  }
+
+  },
+
+});
+
+const WindowsTitlebar = React.createClass({
+
+  minimize() {
+    this.state.window.minimize();
+  },
+
+  maximize() {
+    if (!this.state.window.isMaximized())
+      this.state.window.maximize();
+    else
+      this.state.window.unmaximize();
+  },
+
+  quit() {
+    this.state.window.close();
+  },
+
+  getInitialState() {
+    return {
+      window: remote.getCurrentWindow()
+    };
+  },
+
+  render() {
+
+    return (
+
+      <div id="titlebar" data-platform={this.props.platform}>
+
+        <div className="titlebar--text">
+          <img className="titlebar--icon" src="images/icon.png" />
+          YouWatch
+        </div>
+
+        <div className="titlebar--controls-wrapper">
+          <div className="titlebar--controls-minimize" onClick={this.minimize()}>
+            <span></span>
+          </div>
+
+          <div className="titlebar--controls-maximize" onClick={this.maximize()}>
+            <span></span>
+            <span className="bar2"></span>
+          </div>
+
+          <div className="titlebar--controls-close" onClick={this.quit()}>
+            <span></span>
+            <span className="bar2"></span>
+          </div>
+        </div>
+
+      </div>
+
+    );
+
+  },
+
+});
+
+const Titlebar = React.createClass({
+
+  render() {
+
+    if (this.props.platform === 'darwin')
+      return <MacTitlebar platform={this.props.platform} />;
+    else
+      return <WindowsTitlebar platform={this.props.platform} />;
+
+  },
+
 });
 
 module.exports = Titlebar;
