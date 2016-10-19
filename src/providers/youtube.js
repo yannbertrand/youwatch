8b1a92cfd5ee2bc74bde640cc2bc12fd464df544
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-10-19 23:05:34 +0200

    XO

--- src/providers/youtube.js
+++ src/providers/youtube.js
@@ -3,7 +3,7 @@ module.exports = {
   refresh(callback) {
     console.log(' -> Refreshing YouTube subscriptions');
 
-    setTimeout(_ => callback(0), 2000);
-  }
+    setTimeout(() => callback(0), 2000);
+  },
 
 };
