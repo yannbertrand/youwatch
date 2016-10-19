a344d86dc89700e8be8e0d9493f98907c5ac458d
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-10-19 23:21:54 +0200

    XO

--- src/client/scripts/pages/configuration.react.js
+++ src/client/scripts/pages/configuration.react.js
@@ -1,15 +1,15 @@
-const Switch = require('../components/switch.react.js')
+const Switch = require('../components/switch.react.js');
 
 const ConfigurationPage = React.createClass({
-  toggleDarkTheme: function () {
+  toggleDarkTheme() {
     const darkTheme = document.body.classList.toggle('dark');
 
     localStorage.setItem('darkTheme', castBooleanToString(darkTheme));
 
     this.setState({ darkTheme });
   },
-  changeLayout: function (event) {
-    let layout = event.target.value;
+  changeLayout(event) {
+    const layout = event.target.value;
 
     switch (layout) {
       case 'overlay':
@@ -23,19 +23,19 @@ const ConfigurationPage = React.createClass({
       default:
         document.body.classList.remove('layout-overlay');
         document.body.classList.remove('layout-sticker');
-    };
+    }
 
     localStorage.setItem('layout', layout);
-    this.setState({ layout: layout });
+    this.setState({ layout });
   },
-  getInitialState: () => {
+  getInitialState() {
     return {
       showConsole: false,
       darkTheme : isDarkThemeActive(),
       layout: getActiveLayout(),
     };
   },
-  render: function () {
+  render() {
     return (
       <div className="text-page">
         <h1>Configuration</h1>
@@ -57,8 +57,8 @@ const ConfigurationPage = React.createClass({
             <div className="col-sm-8">
               <div className="select">
                 <select className="form-control" value={this.state.layout} onChange={this.changeLayout}>
-                  <option value="youtube">Youtube</option>  
-                  <option value="overlay">Overlay</option>  
+                  <option value="youtube">Youtube</option>
+                  <option value="overlay">Overlay</option>
                   <option value="sticker">Sticker</option>
                 </select>
               </div>
@@ -67,7 +67,7 @@ const ConfigurationPage = React.createClass({
         </form>
       </div>
     );
-  }
+  },
 });
 
 module.exports = ConfigurationPage;
