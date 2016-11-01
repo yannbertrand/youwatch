13ee8d90c4b2119a5f4137e5baaa38ddb52f1408
Yann Bertrand <yann@bertrand.com>
2016-11-01 22:30:51 +0100

    Change switch label to mode switch

--- src/client/scripts/pages/configuration.react.js
+++ src/client/scripts/pages/configuration.react.js
@@ -50,8 +50,8 @@ const ConfigurationPage = React.createClass({
         <h1>Configuration</h1>
         <form>
           <div className="form-group row">
-            <label className="col-sm-4">Dark Theme</label>
-            <div className="col-sm-8">
+            <label className="col-sm-4 col-form-label">Dark Theme</label>
+            <div className="col-sm-8 col-form-label">
               <Switch
                 isChecked={this.state.darkTheme}
                 onChange={this.toggleDarkTheme}
@@ -63,7 +63,7 @@ const ConfigurationPage = React.createClass({
             </div>
           </div>
           <div className="form-group row">
-            <label className="col-sm-4">Layout</label>
+            <label className="col-sm-4 col-form-label">Layout</label>
             <div className="col-sm-8">
               <div className="select">
                 <select className="form-control" value={this.state.layout} onChange={this.changeLayout}>
@@ -75,17 +75,20 @@ const ConfigurationPage = React.createClass({
             </div>
           </div>
           <div className="form-group row">
-            <label className="col-sm-4">Resize when going fullscreen</label>
-            <div className="col-sm-8">
+            <div className="col-sm-2 col-form-label">Mode</div>
+            <label className="col-sm-2 col-form-label">
+              <span className="pull-right">Fullscreen</span>
+            </label>
+            <div className="col-sm-1 col-form-label">
               <Switch
                 isChecked={this.state.doResize}
                 onChange={this.toggleResize}
+                color="1"
                 size="lg"
-                textOn="I"
-                textOff="O"
                 shape="square"
                 />
             </div>
+            <label className="col-sm-7 col-form-label">Float-on-top</label>
           </div>
         </form>
       </div>
