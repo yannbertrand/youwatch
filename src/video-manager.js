0c46b479e7a09b9cc6293ac764199c3c3ec2b15f
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-12-10 16:28:18 +0100

    Implement video manager init and getVideos methods

--- src/video-manager.js
+++ src/video-manager.js
@@ -1,17 +1,67 @@
+'use strict';
+
+const { app, ipcMain } = require('electron');
+const fs = require('fs');
 const _ = require('lodash');
+const Config = require('electron-config');
 
-const Providers = require('./providers');
+const config = new Config('YouWatch');
 
-module.exports = {
+const Providers = [];
 
-  refresh() {
-    for (const providerName in Providers) {
-      if ({}.hasOwnProperty.call(Providers, providerName)) {
-        Providers[providerName].refresh((nbOfNewVideos) => {
-          console.log('Added ' + nbOfNewVideos + ' video(s) to ' + providerName + ' provider');
-        });
+module.exports = (() => {
+
+  function init(callback) {
+    hydrateProviders((err) => {
+      if (err) {
+        console.error(err);
       }
-    }
-  },
 
-};
+      const options = { config, app, ipcMain };
+
+      Providers.forEach((provider) => {
+        provider.init((err) => {
+          if (err) {
+            console.error(err);
+          }
+
+          callback(err);
+        }, options);
+      });
+    });
+  }
+
+  function getVideos(callback) {
+    Providers.forEach((provider) => {
+      provider.getVideos((err, videos) => {
+        if (err) {
+          console.error(err);
+        }
+
+        callback(err, videos);
+      });
+    });
+  }
+
+  return {
+    init,
+    getVideos,
+  };
+
+})();
+
+function hydrateProviders(callback) {
+  const nodeModules = require('path').join(__dirname, '..', 'node_modules');
+
+  const youwatchProviderRegex = /^youwatch-\w+-provider$/;
+  fs.readdir(nodeModules, (err, files) => {
+    files.forEach((file) => {
+      if (file.match(youwatchProviderRegex)) {
+        // eslint-disable-next-line import/no-dynamic-require
+        Providers.push(require(file));
+      }
+    });
+
+    callback(err);
+  });
+}
