0bc82ddf6bdd881579b141f0b4268c975d6d8f0d
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-12-10 16:29:02 +0100

    Use video manager to get a list of videos

--- src/app.js
+++ src/app.js
@@ -18,85 +18,94 @@ require('electron-debug')();
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
   Windows.openMainWindow();
-  VideoManager.refresh();
 
-  let subscriptions = [];
+  VideoManager.init((err) => {
+    if (err) {
+      console.error(err);
+    }
 
-  ipcMain.on('handshake', (event) => {
-    server.hapi.route({
-      method: 'GET',
-      path:'/youtube/callback',
-      handler: (request, reply) => {
-        event.sender.send('youtube/waiting');
-        YoutubeApi.getToken(request.query.code, (err, token) => {
-          if (err) {
-            event.sender.send('youtube/callbackerror', err);
-            return;
-          }
+    VideoManager.getVideos((err, videos) => {
+      console.log(err, videos);
+    });
 
+    let subscriptions = [];
+
+    ipcMain.on('handshake', (event) => {
+      server.hapi.route({
+        method: 'GET',
+        path:'/youtube/callback',
+        handler: (request, reply) => {
+          event.sender.send('youtube/waiting');
+          YoutubeApi.getToken(request.query.code, (err, token) => {
+            if (err) {
+              event.sender.send('youtube/callbackerror', err);
+              return;
+            }
+
+            event.sender.send('youtube/callback', token);
+            return reply.file(require('path').join('client/authenticated.html'));
+          });
+        },
+      });
+    });
+
+    ipcMain.on('youtube/auth', (event) => {
+      event.sender.send('youtube/waiting');
+
+      YoutubeApi.getAuthUrl((url) => {
+        event.sender.send('youtube/waitingforuser');
+
+        shell.openExternal(url);
+      });
+    });
+
+    ipcMain.on('subscriptions/list', (event) => {
+      if (subscriptions.length > 0)
+        return event.sender.send('subscriptions/list', subscriptions);
+
+      YoutubeApi.getSubscriptions((err, _subscriptions) => {
+        if (err) {
+          console.error(err);
+        } else {
+          subscriptions = _subscriptions;
+          event.sender.send('subscriptions/list', subscriptions);
+        }
+      });
+    });
+
+    // Video
+    ipcMain.on('video/start', (event, id) => {
+      console.log('Video started: ', id);
+    });
+
+    ipcMain.on('video/pause', (event, id) => {
+      console.log('Video paused: ', id);
+    });
+
+    ipcMain.on('video/buffer', (event, id) => {
+      console.log('Video buffering: ', id);
+    });
+
+    /* When something is pasted, if it is a YouTube video, play it */
+    ipcMain.on('video/paste', (event, text) => {
+      if (!youtubeRegex().test(text))
+        return;
+
+      const videoId = youtubeRegex().exec(text)[1];
+      YoutubeApi.getVideo(videoId, (err/* , theVideo */) => {
+        if (err) return;
+        // ToDo
+      });
+    });
+
+    ipcMain.on('app/authenticate', (event) => {
+      YoutubeApi.tryStoredAccessToken((noValidAccessToken, token) => {
+        if (noValidAccessToken) {
+          event.sender.send('youtube/notauthenticated');
+        } else {
           event.sender.send('youtube/callback', token);
-          return reply.file(require('path').join('client/authenticated.html'));
-        });
-      },
-    });
-  });
-
-  ipcMain.on('youtube/auth', (event) => {
-    event.sender.send('youtube/waiting');
-
-    YoutubeApi.getAuthUrl((url) => {
-      event.sender.send('youtube/waitingforuser');
-
-      shell.openExternal(url);
-    });
-  });
-
-  ipcMain.on('subscriptions/list', (event) => {
-    if (subscriptions.length > 0)
-      return event.sender.send('subscriptions/list', subscriptions);
-
-    YoutubeApi.getSubscriptions((err, _subscriptions) => {
-      if (err) {
-        console.error(err);
-      } else {
-        subscriptions = _subscriptions;
-        event.sender.send('subscriptions/list', subscriptions);
-      }
-    });
-  });
-
-  // Video
-  ipcMain.on('video/start', (event, id) => {
-    console.log('Video started: ', id);
-  });
-
-  ipcMain.on('video/pause', (event, id) => {
-    console.log('Video paused: ', id);
-  });
-
-  ipcMain.on('video/buffer', (event, id) => {
-    console.log('Video buffering: ', id);
-  });
-
-  /* When something is pasted, if it is a YouTube video, play it */
-  ipcMain.on('video/paste', (event, text) => {
-    if (!youtubeRegex().test(text))
-      return;
-
-    const videoId = youtubeRegex().exec(text)[1];
-    YoutubeApi.getVideo(videoId, (err/* , theVideo */) => {
-      if (err) return;
-      // ToDo
-    });
-  });
-
-  ipcMain.on('app/authenticate', (event) => {
-    YoutubeApi.tryStoredAccessToken((noValidAccessToken, token) => {
-      if (noValidAccessToken) {
-        event.sender.send('youtube/notauthenticated');
-      } else {
-        event.sender.send('youtube/callback', token);
-      }
+        }
+      });
     });
   });
 
